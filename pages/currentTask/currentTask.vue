dc3f4a2e50de72f35e821f53931e775bc8655748
StarSky1993 <837512976@qq.com>
2019-04-11 22:08:39 +0800

    第一次提交

---
+++ pages/currentTask/currentTask.vue
@@ -0,0 +1,65 @@
+<template>
+    <view class="currentTask">
+        <view class="title">
+            <view class="goback">
+                <text class="eosfont">&#xef07;返回</text>
+            </view>
+            <text>当前任务</text>
+            <text class="eosfont">&#xe670;</text>
+        </view>
+        <view class="list">
+            <view class="item">
+                <image></image>
+                <view>
+                    <view>环境学院教学楼402</view>
+                    <view>燃气问题描述燃气问题描述燃气问题描述燃气问题描述燃气问题描述燃气问题描述</view>
+                    <view>
+                        <text class="eosfont">&#xe60a;</text>
+                        <text>2019-04-26</text>
+                    </view>
+                </view>
+            </view>
+        </view>
+
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    onLoad() {
+
+    },
+    methods: {
+
+    } 
+}
+</script>
+
+<style lang="scss" scoped>
+    .currentTask {
+        .title {
+            display: flex;
+            width: 100%;
+            height: 87upx;
+            line-height: 87upx;
+            justify-content: space-between;
+            padding: 0 21upx;
+            position: absolute;
+            box-sizing: border-box;
+            top: 0;
+            left: 0;
+            background: #fff;
+            .goback {
+                font-size: 30upx;
+            }
+        }
+    }
+    .list {
+        background: #fbfbfb;
+    }
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,102 @@
+<template>
+	<view class="login">
+		<view class="logo">
+			<a href="javascript:;">秦皇岛市北戴河区燃气公司</a>
+		</view>
+		<view class="form">
+			<text class="icon1 eosfont">&#xe768;</text><input type="number" maxlength="11" placeholder="请输入您的手机号" />
+			<text class="icon2 eosfont">&#xe64c;</text><input type="text" class="input2" placeholder="请输入密码" />
+			<button @click="login">登陆</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			login() {
+				uni.navigateTo({
+					url: '/pages/xunjianHome/xunjianHome'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+	.login {
+		width: 100%;
+		height: 1334upx;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background: url('~@/static/images/login/bg.png') center top no-repeat;
+		background-size: 100% 472upx;
+		.logo {
+			width: 100%;
+			height: 316upx;
+			background: url('~@/static/images/login/logo.png') no-repeat;
+			background-size: 100% 316upx;
+			position: absolute;
+			left: 0;
+			top: 320upx;
+			a {
+				display: block;
+				line-height: 316upx;
+				text-indent: -9999upx;
+			}
+		}
+		.form {
+			width: 539upx;
+			position: absolute;
+			left: 109upx;
+			bottom: 288upx;
+			input {
+				height: 67upx;
+				line-height: 67upx;
+				font-size: 28upx;
+				border-bottom: 2upx solid #b7b5b7;
+				padding-left: 100upx;
+				box-sizing: border-box;
+			}
+			.input2 {
+				margin-top: 72upx;
+				
+			}
+			text {
+				position: absolute;
+				left: 25upx;
+			}
+			.icon2 {
+				top: 137upx;	
+				font-size: 45upx;	
+				left: 33upx;		
+			}
+			.icon1 {
+				top: 0upx;
+				font-size: 55upx;
+				
+			}
+			button {
+				width: 537upx;
+				height: 82upx;
+				line-height: 82upx;
+				margin-top: 97upx;
+				background: #187efe;
+				color: #fff;
+				border-radius: 270upx;
+			}
+		}
+	}
+</style>
